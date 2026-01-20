0a6f4ba1e8f7737ccb2291affe8b38d1e9c382de
Phatiski Niry <naphatism@hotmail.com>
2026-01-20 15:34:25 +0700

    Restore '20/1/2569 13:57:42 0.0.18'

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 
 namespace FxImage {
 
-    let tmpns: number[] = [], tmpnl: number = 0;
-    let tbufs: Buffer[] = [];
+    let tmpn: number, tmpn0: number;
+    let tbuf: Buffer;
 
     const NIB_MASK0 = 0xf0;
     const NIB_MASK1 = 0x0f;
@@ -23,25 +23,27 @@ namespace FxImage {
         const fximg = pins.createBuffer(4 + ((1 + (img.width * img.height)) >>> 1));
         fximg.setNumber(NumberFormat.UInt16LE, 0, img.height);
         fximg.setNumber(NumberFormat.UInt16LE, 2, img.width);
-        const tmpn = img.height
-        tmpns.push(tmpn), tbufs.push(pins.createBuffer(tmpn)), tmpnl++;
+        if (!tmpn || (tmpn !== img.height)) tmpn = img.height
+        if (!tbuf || (tbuf.length < tmpn)) tbuf = pins.createBuffer(tmpn);
+        tmpn0 = tmpn;
         for (let x = 0; x < img.width; x++) {
-            img.getRows(x, tbufs[tmpnl - 1]);
-            setRow(fximg, x, tbufs[tmpnl - 1]);
+            img.getRows(x, tbuf);
+            setRow(fximg, x, tbuf);
         }
-        tmpnl--, tbufs.pop(), tmpns.pop();
+        tmpn0 = null;
         return fximg;
     }
 
     export function toImage(fximg: Buffer): Image {
         const img = image.create(fximg.getNumber(NumberFormat.UInt16LE, 2), fximg.getNumber(NumberFormat.UInt16LE, 0));
-        const tmpn = img.height;
-        tmpns.push(tmpn), tbufs.push(pins.createBuffer(tmpn)), tmpnl++;
+        if (!tmpn || (tmpn !== img.height)) tmpn = img.height;
+        if (!tbuf || (tbuf.length < tmpn)) tbuf = pins.createBuffer(tmpn);
+        tmpn0 = tmpn;
         for (let x = 0; x < img.width; x++) {
-            getRow(fximg, x, tbufs[tmpnl - 1]);
-            img.setRows(x, tbufs[tmpnl - 1]);
+            getRow(fximg, x, tbuf);
+            img.setRows(x, tbuf);
         }
-        tmpnl--, tmpns.pop(), tbufs.pop();
+        tmpn0 = null;
         return img.clone();
     }
 
@@ -59,33 +61,35 @@ namespace FxImage {
         const fximgs = pins.createBuffer(4 + ((1 + (allSize.area * imgs.length)) >> 1));
         fximgs.setNumber(NumberFormat.UInt16LE, 0, allSize.height);
         fximgs.setNumber(NumberFormat.UInt16LE, 2, allSize.width);
-        const tmpn = allSize.height;
-        tmpns.push(tmpn), tbufs.push(pins.createBuffer(tmpn)), tmpnl++;
+        if (!tmpn || (tmpn !== allSize.height)) tmpn = allSize.height;
+        if (!tbuf || (tbuf.length < tmpn)) tbuf = pins.createBuffer(tmpn);
+        tmpn0 = tmpn;
         let nw = 0;
         for (const img of imgs) {
             for (let x = 0; x < img.width; x++) {
-                img.getRows(x, tbufs[tmpnl - 1]);
-                setRow(fximgs, nw + x, tbufs[tmpnl - 1])
+                img.getRows(x, tbuf);
+                setRow(fximgs, nw + x, tbuf)
             }
             nw += allSize.width
         }
-        tmpnl--, tmpns.pop(), tbufs.pop();
+        tmpn0 = null;
         return fximgs;
     }
 
     export function toFrame(fximgs: Buffer) {
         const imgs = []
         const img = image.create(fximgs.getNumber(NumberFormat.UInt16LE, 2), fximgs.getNumber(NumberFormat.UInt16LE, 0));
-        const tmpn = img.height;
-        tmpns.push(tmpn), tbufs.push(pins.createBuffer(tmpn)), tmpnl++;
+        if (!tmpn || (tmpn !== img.height)) tmpn = img.height;
+        if (!tbuf || (tbuf.length < tmpn)) tbuf = pins.createBuffer(tmpn);
+        tmpn0 = tmpn;
         for (let nw = 0; (((1 + (nw * img.height)) >> 1) + 4) < fximgs.length; nw += img.width) {
             for (let x = 0; x < img.width; x++) {
-                getRow(fximgs, nw + x, tbufs[tmpnl - 1]);
-                img.setRows(x, tbufs[tmpnl - 1]);
+                getRow(fximgs, nw + x, tbuf);
+                img.setRows(x, tbuf);
             }
             imgs.push(img.clone())
         }
-        tmpnl--, tmpns.pop(), tbufs.pop();
+        tmpn0 = null;
         return imgs.slice();
     }
 
@@ -111,7 +115,7 @@ namespace FxImage {
     }
 
     export function getRow(fximg: Buffer, x: number, dst: Buffer) {
-        const h0 = tmpnl > 0 ? tmpns[tmpnl - 1] : fximg.getNumber(NumberFormat.UInt16LE, 0);
+        const h0 = tmpn0 != null ? tmpn0 : fximg.getNumber(NumberFormat.UInt16LE, 0);
         const len = Math.min(dst.length, h0);
         if (len < 1) return;
         let i = x * h0,
@@ -135,8 +139,8 @@ namespace FxImage {
     }
 
     export function setRow(fximg: Buffer, x: number, src: Buffer) {
-        const h0 = tmpnl > 0 ? tmpns[tmpnl - 1] : fximg.getNumber(NumberFormat.UInt16LE, 0)
-        const len = Math.min(src.length, h0);
+        const h0 = tmpn0 != null ? tmpn0 : fximg.getNumber(NumberFormat.UInt16LE, 0)
+        const len = Math.min(src.length, tmpn0);
         if (len < 1) return;
         let i = x * h0,
             y = 0;
@@ -159,59 +163,115 @@ namespace FxImage {
         }
     }
 }
-/*
-// test zone: mutitask test
+/* // test zone
 scene.setBackgroundColor(1)
+const imgfxa = FxImage.fromImage(img`
+    ..........666666666666..........
+    ........6667777777777666........
+    ......66677777777777777666......
+    .....6677777779999777777766.....
+    ....667777779966669977777766....
+    ....677777799668866117777776....
+    ...66777779966877861197777766...
+    ...66777799668677686699777766...
+    ...88777796688888888669777788...
+    ...88777788888888888888777788...
+    ...88977888679999997688877988...
+    ...88977886777777777768877988...
+    ...88997777777777777777779988...
+    ...88799777777777777777711788...
+    ...88679997777777777779117688...
+    ..cc866679999999999999976668cc..
+    .ccbc6666679999999999766666cbcc.
+    .fcbcc66666666666666666666ccbcf.
+    .fcbbcc666666666666666666ccbdcf.
+    .f8bbbccc66666666666666cccbddcf.
+    .f8cbbbbccccccccccccccccbdddbcf.
+    .f8ccbbbbbccccccccccccb111ddccf.
+    .f6ccccbbbddddddddddddd111dcccf.
+    .f6ccccccbbddddddddddddddbbcccf.
+    .f6cccccccccccccbbbbbbbbbdbcccf.
+    ..f6cccccccccbbbbbbbbbbbddbccf..
+    ..f6cccccccccbbbbbbbbbbbddbccf..
+    ..ff6ccccccccbbbbbbbbbbbddbcff..
+    ...ff6cccccccbbbbbbbbbbbddbff...
+    ....ffcccccccbbbbbbbbbbbdbff....
+    ......ffccccbbbbbbbbbbbbff......
+    ........ffffffffffffffff........
+`)
 
-let mySpriteA = sprites.create(img`
-    4 4 4 . . 4 4 4 4 4 . . . . . .
-    4 5 5 4 4 5 5 5 5 5 4 4 . . . .
-    b 4 5 5 1 5 1 1 1 5 5 5 4 . . .
-    . b 5 5 5 5 1 1 5 5 1 1 5 4 . .
-    . b d 5 5 5 5 5 5 5 5 1 1 5 4 .
-    b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 .
-    c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4
-    c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4
-    c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4
-    c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4
-    . c 4 5 5 5 5 d d d 5 5 5 5 5 b
-    . c 4 d 5 4 5 d 4 4 d 5 5 5 4 c
-    . . c 4 4 d 4 4 4 4 4 d d 5 d c
-    . . . c 4 4 4 4 4 4 4 4 5 5 5 4
-    . . . . c c b 4 4 4 b b 4 5 4 4
-    . . . . . . c c c c c c b b 4 .
-`, SpriteKind.Player)
-mySpriteA.x += 32
-let mySpriteB = sprites.create(img`
-    . . . . . . . e c 7 . . . . . .
-    . . . . e e e c 7 7 e e . . . .
-    . . c e e e e c 7 e 2 2 e e . .
-    . c e e e e e c 6 e e 2 2 2 e .
-    . c e e e 2 e c c 2 4 5 4 2 e .
-    c e e e 2 2 2 2 2 2 4 5 5 2 2 e
-    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e
-    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e
-    . e e e 2 2 2 2 2 2 2 2 2 4 e .
-    . 2 e e 2 2 2 2 2 2 2 2 4 2 e .
-    . . 2 e e 2 2 2 2 2 4 4 2 e . .
-    . . . 2 2 e e 4 4 4 2 e e . . .
-    . . . . . 2 2 e e e e . . . . .
-`, SpriteKind.Player)
-mySpriteB.x -= 32
+const imgfxas = FxImage.fromFrame([
+    img`
+        ..........666666666666..........
+        ........6667777777777666........
+        ......66677777777777777666......
+        .....6677777779999777777766.....
+        ....667777779966669977777766....
+        ....677777799668866117777776....
+        ...66777779966877861197777766...
+        ...66777799668677686699777766...
+        ...88777796688888888669777788...
+        ...88777788888888888888777788...
+        ...88977888679999997688877988...
+        ...88977886777777777768877988...
+        ...88997777777777777777779988...
+        ...88799777777777777777711788...
+        ...88679997777777777779117688...
+        ..cc866679999999999999976668cc..
+        .ccbc6666679999999999766666cbcc.
+        .fcbcc66666666666666666666ccbcf.
+        .fcbbcc666666666666666666ccbdcf.
+        .f8bbbccc66666666666666cccbddcf.
+        .f8cbbbbccccccccccccccccbdddbcf.
+        .f8ccbbbbbccccccccccccb111ddccf.
+        .f6ccccbbbddddddddddddd111dcccf.
+        .f6ccccccbbddddddddddddddbbcccf.
+        .f6cccccccccccccbbbbbbbbbdbcccf.
+        ..f6cccccccccbbbbbbbbbbbddbccf..
+        ..f6cccccccccbbbbbbbbbbbddbccf..
+        ..ff6ccccccccbbbbbbbbbbbddbcff..
+        ...ff6cccccccbbbbbbbbbbbddbff...
+        ....ffcccccccbbbbbbbbbbbdbff....
+        ......ffccccbbbbbbbbbbbbff......
+        ........ffffffffffffffff........
+    `,
+    img`
+        ................................
+        ................................
+        ................................
+        ................................
+        ................................
+        ..........888888888888..........
+        ........8887777777777888........
+        ......88877666666666677888......
+        .....8877666667777666667788.....
+        ....887666667788887766666788....
+        ....866666677888888996666678....
+        ...88666667788877889976666688...
+        ...88666677888677688877666688...
+        ...88666778888888888887766688...
+        ...88667788888888888888776688...
+        ..cc866788866777777668887668cc..
+        .ccbc8668866666666666688668cbcc.
+        .fcbcc86666666666666666668ccbcf.
+        .fcbbcc886666666666666688ccbdcf.
+        .f8bbbccc88888888888888cccbddcf.
+        .f8cbbbbccccccccccccccccbdddbcf.
+        .f8ccbbbbbccccccccccccb11dddccf.
+        .f6ccccbbbdddddddddddd111ddcccf.
+        .f6ccccccbbddddddddddd11dbbcccf.
+        .f6cccccccccccccbbbbbbbbbdbcccf.
+        ..f6cccccccccbbbbbbbbbbbddbccf..
+        ..f6cccccccccbbbbbbbbbbbddbccf..
+        ..ff6ccccccccbbbbbbbbbbbddbcff..
+        ...ff6cccccccbbbbbbbbbbbddbff...
+        ....ffcccccccbbbbbbbbbbbdbff....
+        ......ffccccbbbbbbbbbbbbff......
+        ........ffffffffffffffff........
+    `
+])
 
-basic.forever(() => {
-    const imgfxa = FxImage.fromImage(mySpriteA.image)
-    mySpriteA.image.fill(0)
-    //basic.pause(20)
-    mySpriteA.setImage(FxImage.toImage(imgfxa))
-})
-basic.forever(() => {
-    const imgfxb = FxImage.fromImage(mySpriteB.image)
-    mySpriteB.image.fill(0)
-    //basic.pause(20)
-    mySpriteB.setImage(FxImage.toImage(imgfxb))
-})
+let mySprite = sprites.create(FxImage.toImage(imgfxa), SpriteKind.Player)
+
+// animation.runImageAnimation(mySprite, FxImage.toFrame(imgfxas), 100, true)
 */
