329c92faca9a3cc017e74178e64df6a33e3fd5c4
Phatiski Niry <naphatism@hotmail.com>
2026-02-17 19:50:33 +0700

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,24 +2,21 @@
 
 /*
 const imgfxa = fximg.fromImage(img`
-    . . . 1 1 1 2 2 2 3 3 3
-    . . . 1 1 1 2 2 2 3 3 3
-    . . . 1 1 1 2 2 2 3 3 3
-    4 4 4 5 5 5 6 6 6 7 7 7
-    4 4 4 5 5 5 6 6 6 7 7 7
-    4 4 4 5 5 5 6 6 6 7 7 7
-    8 8 8 9 9 9 a a a b b b
-    8 8 8 9 9 9 a a a b b b
-    8 8 8 9 9 9 a a a b b b
-    c c c d d d e e e f f f
-    c c c d d d e e e f f f
-    c c c d d d e e e f f f
+    1 1 1 2 2 2 3 3 3 4 4 4 5 5 5
+    1 1 1 2 2 2 3 3 3 4 4 4 5 5 5
+    1 1 1 2 2 2 3 3 3 4 4 4 5 5 5
+    6 6 6 7 7 7 8 8 8 9 9 9 a a a
+    6 6 6 7 7 7 8 8 8 9 9 9 a a a
+    6 6 6 7 7 7 8 8 8 9 9 9 a a a
+    b b b c c c d d d e e e f f f
+    b b b c c c d d d e e e f f f
+    b b b c c c d d d e e e f f f
 `)
 
 const mySprite = sprites.create(fximg.toImage(imgfxa), 0);
 
 if (1) {
-    imgfxa[4] = 0x00
+    imgfxa[6] = 0x00
     mySprite.setImage(fximg.toImage(imgfxa))
 }
 */
